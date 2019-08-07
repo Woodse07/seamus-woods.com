a105cce525e4b18154574f162c4df87b66e2a051
Seamus Woods <seamus.woods@hpe.com>
2019-08-07 09:32:18 +0100

    New website live

--- js/contact_me.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#bt1').click(function() {
-        $('#fr1').attr('action',
-                       'mailto:sewoods@tcd.ie?subject=' +
-                       $('#subject').val() + '&body=' + $('#message').val());
-        $('#fr1').submit();
-    });
-});
